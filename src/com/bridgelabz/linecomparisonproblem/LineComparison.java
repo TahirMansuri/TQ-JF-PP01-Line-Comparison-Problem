6acc8176d71ce2c12fa267c424f2114ee2f5c88d
TahirMansuri <tprocks786@gmail.com>
2021-12-29 13:37:44 +0530

    ADDED: Line Compariosn Problem in Java

---
+++ src/com/bridgelabz/linecomparisonproblem/LineComparison.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.linecomparisonproblem;
+
+public class LineComparison {
+    public static void main(String[] args) {
+
+        //Welcome Message for Initial Purpose
+        System.out.println("Welcome to Line Comparison Problem Developed by Tahir Mansuri.");
+    }
+}
